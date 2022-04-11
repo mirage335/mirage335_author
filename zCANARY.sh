24561056f633e30f2309a347c225534eb01b270a
mirage335 <spamfreemirage335@gmail.com>
2022-04-11 19:00:40 -0400

    Canary.

--- zCANARY.sh
+++ zCANARY.sh
@@ -480,6 +480,6 @@ fi
 #___ ### NOTICE ### CANARY ### NOTICE ### ___
 # Author 'mirage335', as of the specified date here, if any intermediaries have not interfered, claims to not be under duress.
 # Any inference from this canary, especially whether any other such or different canary may or may not exist with or without more recent information or any other contingencies, may or may not be invalid.
-#2022-04-01T00:08:10,627276318-04:00
-#00:08:10
-#2022-04-01
+#2022-04-11T19:00:27,417879138-04:00
+#19:00:27
+#2022-04-11
